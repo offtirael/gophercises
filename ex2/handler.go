46ed381dbc353bfa96763ca0a28dc16e52d9b3e2
Vladislav Volkov <volkov@appvelox.ru>
2022-01-08 22:17:53 +0300

    Ex2 part one - done

--- ex2/handler.go
+++ ex2/handler.go
@@ -12,7 +12,13 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
-	return nil
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if route, ok := pathsToUrls[r.URL.Path]; ok {
+			http.Redirect(w, r, route, http.StatusSeeOther)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	})
 }
 
 // YAMLHandler will parse the provided YAML and then return
